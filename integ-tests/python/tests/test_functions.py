5c13e9b0912fb7a71bc3cbf1ea5900ffc9509b76
Antonio Sarosi <sarosiantonio@gmail.com>
2024-10-26 03:59:13 +0100

    Fix python test function name

--- integ-tests/python/tests/test_functions.py
+++ integ-tests/python/tests/test_functions.py
@@ -29,7 +29,7 @@ from ..baml_client.types import (
     StringToClassEntry,
     MalformedConstraints2,
     LiteralClassHello,
-    LiteralClassOne
+    LiteralClassOne,
 )
 import baml_client.types as types
 from ..baml_client.tracing import trace, set_tags, flush, on_log_event
@@ -121,7 +121,9 @@ class TestAllInputs:
         assert res.primary.value.checks["valid_email"].status == "succeeded"
         assert res.secondary is None
 
-        res = await b.ExtractContactInfo("Reach me at help@boundaryml.com, or 111-222-3333 if needed.")
+        res = await b.ExtractContactInfo(
+            "Reach me at help@boundaryml.com, or 111-222-3333 if needed."
+        )
         assert res.primary.value is not None
         assert res.primary.value.checks["valid_email"].status == "succeeded"
         assert res.secondary.value.checks["valid_phone_number"].status == "succeeded"
@@ -200,7 +202,7 @@ class TestAllInputs:
 
     @pytest.mark.asyncio
     async def test_class_with_literal_prop(self):
-        res = await b.FnLiteralInputOutput(input=LiteralClassHello(prop="hello"))
+        res = await b.FnLiteralClassInputOutput(input=LiteralClassHello(prop="hello"))
         assert isinstance(res, LiteralClassHello)
 
     @pytest.mark.asyncio
@@ -905,7 +907,12 @@ async def test_dynamic_types_existing_enum():
 @pytest.mark.asyncio
 async def test_dynamic_literals():
     tb = TypeBuilder()
-    animals = tb.union([tb.literal_string(animal.upper()) for animal in ["giraffe", "elephant", "lion"]])
+    animals = tb.union(
+        [
+            tb.literal_string(animal.upper())
+            for animal in ["giraffe", "elephant", "lion"]
+        ]
+    )
     tb.Person.add_property("animalLiked", animals)
     res = await b.ExtractPeople(
         "My name is Harrison. My hair is black and I'm 6 feet tall. I'm pretty good around the hoop. I like giraffes.",
@@ -1177,8 +1184,6 @@ async def test_descriptions():
         "donkey kong"
     )  # Assuming this returns a Pydantic model
 
-
-
     # Check Schema values
     assert res.prop1 == "one"
 
@@ -1240,9 +1245,9 @@ async def test_arg_exceptions():
 
     with pytest.raises(errors.BamlInvalidArgumentError):
         _ = await b.TestCaching(
-            111, # type: ignore -- intentionally passing an int instead of a string
-            ".."
-        ) 
+            111,  # type: ignore -- intentionally passing an int instead of a string
+            "..",
+        )
 
     with pytest.raises(errors.BamlClientError):
         cr = baml_py.ClientRegistry()
@@ -1295,6 +1300,7 @@ async def test_baml_validation_error_format():
             raise e
     assert "Failed to parse" in str(excinfo)
 
+
 @pytest.mark.asyncio
 async def test_no_stream_big_integer():
     stream = b.stream.StreamOneBigNumber(digits=12)
@@ -1304,6 +1310,7 @@ async def test_no_stream_big_integer():
     res = await stream.get_final_response()
     for msg in msgs:
         assert True if msg is None else msg == res
+
 
 @pytest.mark.asyncio
 async def test_no_stream_object_with_numbers():
@@ -1319,9 +1326,10 @@ async def test_no_stream_object_with_numbers():
         assert True if msg.a is None else msg.a == res.a
         assert True if msg.b is None else msg.b == res.b
 
+
 @pytest.mark.asyncio
 async def test_no_stream_compound_object():
-    stream = b.stream.StreamingCompoundNumbers(digits = 12, yapping=False)
+    stream = b.stream.StreamingCompoundNumbers(digits=12, yapping=False)
     msgs: List[partial_types.CompoundBigNumbers] = []
     async for msg in stream:
         msgs.append(msg)
@@ -1330,16 +1338,17 @@ async def test_no_stream_compound_object():
         if msg.big is not None:
             assert True if msg.big.a is None else msg.big.a == res.big.a
             assert True if msg.big.b is None else msg.big.b == res.big.b
-        for (msgEntry, resEntry) in zip(msg.big_nums, res.big_nums):
+        for msgEntry, resEntry in zip(msg.big_nums, res.big_nums):
             assert True if msgEntry.a is None else msgEntry.a == resEntry.a
             assert True if msgEntry.b is None else msgEntry.b == resEntry.b
         if msg.another is not None:
             assert True if msg.another.a is None else msg.another.a == res.another.a
             assert True if msg.another.b is None else msg.another.b == res.another.b
 
+
 @pytest.mark.asyncio
 async def test_no_stream_compound_object_with_yapping():
-    stream = b.stream.StreamingCompoundNumbers(digits = 12, yapping=True)
+    stream = b.stream.StreamingCompoundNumbers(digits=12, yapping=True)
     msgs: List[partial_types.CompoundBigNumbers] = []
     async for msg in stream:
         msgs.append(msg)
@@ -1348,12 +1357,13 @@ async def test_no_stream_compound_object_with_yapping():
         if msg.big is not None:
             assert True if msg.big.a is None else msg.big.a == res.big.a
             assert True if msg.big.b is None else msg.big.b == res.big.b
-        for (msgEntry, resEntry) in zip(msg.big_nums, res.big_nums):
+        for msgEntry, resEntry in zip(msg.big_nums, res.big_nums):
             assert True if msgEntry.a is None else msgEntry.a == resEntry.a
             assert True if msgEntry.b is None else msgEntry.b == resEntry.b
         if msg.another is not None:
             assert True if msg.another.a is None else msg.another.a == res.another.a
             assert True if msg.another.b is None else msg.another.b == res.another.b
+
 
 @pytest.mark.asyncio
 async def test_differing_unions():
@@ -1362,16 +1372,19 @@ async def test_differing_unions():
     res = await b.DifferentiateUnions({"tb": tb})
     assert isinstance(res, OriginalB)
 
+
 @pytest.mark.asyncio
 async def test_return_failing_assert():
     with pytest.raises(errors.BamlValidationError):
         msg = await b.ReturnFailingAssert(1)
+
 
 @pytest.mark.asyncio
 async def test_parameter_failing_assert():
     with pytest.raises(errors.BamlInvalidArgumentError):
         msg = await b.ReturnFailingAssert(100)
         assert msg == 103
+
 
 @pytest.mark.asyncio
 async def test_failing_assert_can_stream():
