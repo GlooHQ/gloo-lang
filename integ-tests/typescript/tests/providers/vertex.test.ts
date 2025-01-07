8199507d1acd0b0c67adaca0793c48071baecb6e
Sam Lijin <sam@boundaryml.com>
2025-01-06 18:25:43 -0800

    also delete integ test changes

--- integ-tests/typescript/tests/providers/vertex.test.ts
+++
@@ -1,8 +0,0 @@
-import { b } from '../test-setup'
-
-describe('Vertex Provider', () => {
-  it('should support vertex', async () => {
-    const res = await b.TestVertex('Donkey Kong')
-    expect(res.toLowerCase()).toContain('donkey')
-  })
-})
