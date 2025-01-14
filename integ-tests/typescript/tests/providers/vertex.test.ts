461dcccabb3068642fc2bc4997d121a2e46e975f
Sam Lijin <sam@boundaryml.com>
2025-01-13 17:31:03 -0800

    make it work on non-wasm
    in theory it works on wasm now
    
    ok, i think this now all works
    
    more fixes
    
    canary

--- integ-tests/typescript/tests/providers/vertex.test.ts
+++ integ-tests/typescript/tests/providers/vertex.test.ts
@@ -1,7 +1,7 @@
 import { b } from '../test-setup'
 
 describe('Vertex Provider', () => {
-  it('should support vertex', async () => {
+  it.only('should support vertex', async () => {
     const res = await b.TestVertex('Donkey Kong')
     expect(res.toLowerCase()).toContain('donkey')
   })
